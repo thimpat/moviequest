bb26b20b4b4409d46cf5e3574e1a94f3355351cf
Patrice <thimpat@gmail.com>
2021-05-16 14:51:30 +0100

    FEAT: AC3 Initiate search on pressing Search button

--- src/app/components/SearchBar/index.jsx
+++ src/app/components/SearchBar/index.jsx
@@ -1,12 +1,23 @@
 import { Container } from "./styles";
 import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import { useState } from "react";
 
 function SearchBar() {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [currentTextBoxContent, setCurrentTextBoxContent] = useState("");
+  const [isSearchInitiated, setIsSearchInitiated] = useState(false);
+
+  const onStartSearch = () => {
+    setCurrentTextBoxContent(searchPhrase);
+    setIsSearchInitiated(true);
+  };
+
   return (
     <Container>
       <Navbar bg="light" expand="lg">
         <Navbar.Brand id="app-name">Movie Quest</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Nav.Link>{isSearchInitiated ? `Searching... ${currentTextBoxContent}` : ""}</Nav.Link>
         <Navbar.Collapse>
           <Nav className="mr-auto" />
           <Form inline>
@@ -17,8 +28,14 @@ function SearchBar() {
               type="text"
               placeholder="Search"
               className="mr-sm-2"
+              onChange={e => setSearchPhrase(e.target.value)}
             />
-            <Button id="search-button" data-testid="search-button" variant="outline-success">
+            <Button
+              id="search-button"
+              data-testid="search-button"
+              variant="outline-success"
+              onClick={onStartSearch}
+            >
               Search
             </Button>
           </Form>
