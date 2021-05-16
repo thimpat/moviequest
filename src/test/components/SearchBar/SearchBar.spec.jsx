d10680b3fbb1af4edc89d14c5199f61eaf01df35
Patrice <thimpat@gmail.com>
2021-05-16 15:19:16 +0100

    TEST: Add tests for AC3

--- src/test/components/SearchBar/SearchBar.spec.jsx
+++ src/test/components/SearchBar/SearchBar.spec.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import SearchBar from "../../../app/components/SearchBar";
 
@@ -20,5 +20,20 @@ describe("The SearchBar component", () => {
     render(<SearchBar />);
     const linkElement = screen.getByPlaceholderText("Search");
     expect(document.activeElement).toEqual(linkElement);
+  });
+
+  it("should initiate a search when the search button is pressed", () => {
+    render(<SearchBar />);
+    // Add a text to the input
+    const inputBox = screen.getByPlaceholderText("Search");
+    fireEvent.change(inputBox, { target: { value: "Good Day" } });
+    fireEvent.blur(inputBox);
+
+    // Press the search button
+    const searchButton = screen.getByText("Search");
+    fireEvent.click(searchButton);
+
+    const linkElement = screen.getByText("Searching... Good Day");
+    expect(linkElement).toBeInTheDocument();
   });
 });
